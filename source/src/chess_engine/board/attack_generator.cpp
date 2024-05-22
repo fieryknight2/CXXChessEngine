87a37cfd228a6c2cc6256d54670fa29bfcc36f29
Fierynight <84808424+fieryknight2@users.noreply.github.com>
2024-05-22 10:27:55 -0500

    Implemented the get attack functions

--- source/src/chess_engine/board/attack_generator.cpp
+++ source/src/chess_engine/board/attack_generator.cpp
@@ -25,6 +25,7 @@
  * @date 05/21/2024
  *****************************************************************************/
 #include "chess_engine/board/move_generator.h"
+#include "chess_engine/constants.h"
 
 
 /** Generates all legal moves on the board for the given piece
@@ -37,22 +38,36 @@
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_attacks(std::stack<int>* attacks, const char *board, const int piece) {
-    std::stack<int> temp;
+void get_attacks(std::stack<int> *attacks, const int *board, const int piece) {
     switch (board[piece]) {
-        case 0: // Empty square
-            break;
-        case 1: // White pawn
-            get_pawn_attacks(attacks, false, piece);
-            break;
-        case -1: // Black pawn
+        case WHITE_PAWN: // White pawn
             get_pawn_attacks(attacks, true, piece);
             break;
-        case 2: // White knight
+        case BLACK_PAWN: // Black pawn
+            get_pawn_attacks(attacks, false, piece);
+            break;
+        case WHITE_KNIGHT: // White knight
+        case BLACK_KNIGHT: // Black knight
             get_knight_attacks(attacks, piece);
             break;
-        case -2: // Black knight
-            get_knight_attacks(attacks, piece);
+        case WHITE_BISHOP: // White bishop
+        case BLACK_BISHOP: // Black bishop
+            get_bishop_attacks(attacks, board, piece);
+            break;
+        case WHITE_ROOK: // White rook
+        case BLACK_ROOK: // Black rook
+            get_rook_attacks(attacks, board, piece);
+            break;
+        case WHITE_QUEEN: // White queen
+        case BLACK_QUEEN: // Black queen
+            get_queen_attacks(attacks, board, piece);
+            break;
+        case WHITE_KING: // White king
+        case BLACK_KING: // Black king
+            get_king_attacks(attacks, piece);
+            break;
+        case EMPTY: // Empty square
+        default:
             break;
     }
 }
@@ -68,7 +83,7 @@ void get_attacks(std::stack<int>* attacks, const char *board, const int piece) {
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_pawn_attacks(std::stack<int>* attacks, const char *board, const int piece) {
+void get_pawn_attacks(std::stack<int> *attacks, const int *board, const int piece) {
     // Remember piece % 8 is the file
     // piece // 8 is the rank
     // Pawns aren't allowed on the last rank
@@ -83,8 +98,7 @@ void get_pawn_attacks(std::stack<int>* attacks, const char *board, const int pie
         if (piece % 8 < 7) {
             attacks->push(piece - 7); // Right side
         }
-    }
-    else { // White pawn
+    } else { // White pawn
         if (piece % 8 > 0) {
             attacks->push(piece + 7); // Right side
         }
@@ -105,7 +119,7 @@ void get_pawn_attacks(std::stack<int>* attacks, const char *board, const int pie
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_pawn_attacks(std::stack<int>* attacks, bool color, int piece) {
+void get_pawn_attacks(std::stack<int> *attacks, bool color, int piece) {
     if (!attacks or piece < 8 or piece > 55) {
         return;
     }
@@ -117,8 +131,7 @@ void get_pawn_attacks(std::stack<int>* attacks, bool color, int piece) {
         if (piece % 8 < 7) {
             attacks->push(piece - 7); // Right side
         }
-    }
-    else { // White pawn
+    } else { // White pawn
         if (piece % 8 > 0) {
             attacks->push(piece + 7); // Right side
         }
@@ -137,7 +150,55 @@ void get_pawn_attacks(std::stack<int>* attacks, bool color, int piece) {
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_knight_attacks(std::stack<int>* attacks, int piece) {
+void get_knight_attacks(std::stack<int> *attacks, int piece) {
+    // Code ripped out of my python implementation
+    if (piece / 8 > 1) { // C++ automatically converts int / int to int, no integer division necessary
+        // validate bottom far
+        if (piece % 8 > 0) {
+            // validate right close
+            attacks->push(piece - 17);
+        }
+        if (piece % 8 < 7) {
+            // validate left close
+            attacks->push(piece - 15);
+        }
+    }
+
+    if (piece / 8 > 0) {
+        // validate bottom close
+        if (piece % 8 > 1) {
+            // validate right far
+            attacks->push(piece - 10);
+        }
+        if (piece % 8 < 6) {
+            // validate left far
+            attacks->push(piece - 6);
+        }
+    }
+
+    if (piece / 8 < 7) {
+        // validate top close
+        if (piece % 8 > 1) {
+            // validate right far
+            attacks->push(piece + 6);
+        }
+        if (piece % 8 < 6) {
+            // validate left far
+            attacks->push(piece + 10);
+        }
+    }
+
+    if (piece / 8 < 6) {
+        // validate top far
+        if (piece % 8 > 0) {
+            // validate right close
+            attacks->push(piece + 15);
+        }
+        if (piece % 8 < 7) {
+            // validate left close
+            attacks->push(piece + 17);
+        }
+    }
 }
 
 
@@ -150,7 +211,51 @@ void get_knight_attacks(std::stack<int>* attacks, int piece) {
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_bishop_attacks(std::stack<int>* attacks, char *board, int piece) {
+void get_bishop_attacks(std::stack<int> *attacks, const int *board, int piece) {
+    int index = piece;
+
+    while (index - 9 >= 0 and index % 8 != 0) {
+        // Bottom Right
+        index -= 9;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index - 7 >= 0 and index % 8 != 7) {
+        // Bottom Left
+        index -= 7;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 7 < 64 and index % 8 != 0) {
+        // Top Left
+        index += 7;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 9 < 64 and index % 8 != 7) {
+        // Top Right
+        index += 9;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
 }
 
 /** Generates all legal moves on the board for the given rook
@@ -162,7 +267,52 @@ void get_bishop_attacks(std::stack<int>* attacks, char *board, int piece) {
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_rook_attacks(std::stack<int>* attacks, char *board, int piece) {
+void get_rook_attacks(std::stack<int> *attacks, const int *board, int piece) {
+    int index = piece;
+    const int rank_min = (piece / 8) * 8;
+
+    while (index - 8 >= 0) {
+        // Left
+        index -= 8;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index - 1 >= rank_min) {
+        // Right
+        index -= 1;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index < rank_min + 7) {
+        // Left
+        index += 1;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 8 < 64) {
+        // Right
+        index += 8;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
 }
 
 /** Generates all legal moves on the board for the given queen
@@ -174,7 +324,99 @@ void get_rook_attacks(std::stack<int>* attacks, char *board, int piece) {
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_queen_attacks(std::stack<int>* attacks, char *board, int piece) {
+void get_queen_attacks(std::stack<int> *attacks, const int *board, int piece) {
+    int index = piece;
+    const int rank_min = (piece / 8) * 8;
+
+    // Rook Code
+    while (index - 8 >= 0) {
+        // Left
+        index -= 8;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index - 1 >= rank_min) {
+        // Right
+        index -= 1;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index < rank_min + 7) {
+        // Left
+        index += 1;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 8 < 64) {
+        // Right
+        index += 8;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+
+    // Bishop Code
+    index = piece;
+    while (index - 9 >= 0 and index % 8 != 0) {
+        // Bottom Right
+        index -= 9;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index - 7 >= 0 and index % 8 != 7) {
+        // Bottom Left
+        index -= 7;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 7 < 64 and index % 8 != 0) {
+        // Top Left
+        index += 7;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
+
+    index = piece;
+    while (index + 9 < 64 and index % 8 != 7) {
+        // Top Right
+        index += 9;
+        attacks->push(index);
+
+        if (board[index] != EMPTY) {
+            break;
+        }
+    }
 }
 
 /** Generates all legal moves on the board for the given king
@@ -182,9 +424,43 @@ void get_queen_attacks(std::stack<int>* attacks, char *board, int piece) {
  * See documentation for legal_king_moves() for more information
  *
  * @param attacks Stacks to add attacks too
- * @param board List of all pieces and their locations on the board
  * @param piece The piece to generate legal moves for
  * @return All possible legal moves for the given piece
  */
-void get_king_attacks(std::stack<int>* attacks, char *board, int piece) {
+void get_king_attacks(std::stack<int> *attacks, int piece) {
+    if (piece > 7) {
+        // validate bottom
+        if (piece % 8 > 0) {
+            attacks->push(piece - 9); // bottom right corner
+        }
+
+        attacks->push(piece - 8); // bottom
+
+        if (piece % 8 < 7) {
+            attacks->push(piece - 7); // bottom left corner
+        }
+    }
+
+    if (piece % 8 > 0) {
+        // validate right
+        attacks->push(piece - 1);
+    }
+
+    if (piece % 8 < 7) {
+        // validate left
+        attacks->push(piece + 1);
+    }
+
+    // validate top
+    if (piece < 56) {
+        if (piece % 8 > 0) {
+            attacks->push(piece + 7); // top right corner
+        }
+
+        attacks->push(piece + 8); // top
+
+        if (piece % 8 < 7) {
+            attacks->push(piece + 9); // top left corner
+        }
+    }
 }
