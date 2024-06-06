11f7640f1096dd64ac19755610c27d20cc6276f5
Fierynight <84808424+fieryknight2@users.noreply.github.com>
2024-06-06 12:29:58 -0500

    Added more testing functions

--- tests/chess_engine/low_level_test_functions.cpp
+++ tests/chess_engine/low_level_test_functions.cpp
@@ -5,9 +5,14 @@
  */
 #include "low_level_test_functions.h"
 
-bool stack_contains(std::stack<int> stack, int value) {
-    while (!stack.empty()) {
-        if (stack.top() == value) {
+#include <cstdint>
+
+bool stack_contains(std::stack<int> stack, int value)
+{
+    while (!stack.empty())
+    {
+        if (stack.top() == value)
+        {
             return true;
         }
         stack.pop();
@@ -16,16 +21,20 @@ bool stack_contains(std::stack<int> stack, int value) {
     return false;
 }
 
-bool stack_is_equal(std::vector<int> stack1, const std::stack<int>& stack2) {
-    if (stack1.size() != stack2.size()) {
+bool stack_is_equal(std::vector<int> stack1, const std::stack<int> &stack2)
+{
+    if (stack1.size() != stack2.size())
+    {
         std::cout << "Diff stack sizes are not equal." << std::endl;
         return false;
     }
 
     int index = static_cast<int>(stack1.size());
-    while (!stack1.empty()) {
+    while (!stack1.empty())
+    {
         --index;
-        if (!stack_contains(stack2, stack1.back())) {
+        if (!stack_contains(stack2, stack1.back()))
+        {
             std::cout << "Diff at index " << index << ", value: " << stack1.back() << " is not found." << std::endl;
             return false;
         }
@@ -35,27 +44,108 @@ bool stack_is_equal(std::vector<int> stack1, const std::stack<int>& stack2) {
     return true;
 }
 
-void empty_stack(std::stack<int> *stack) {
-    while (!stack->empty()) {
+void empty_stack(std::stack<int> *stack)
+{
+    while (!stack->empty())
+    {
         stack->pop();
     }
 }
 
-std::string display_stack(std::stack<int> stack) {
+std::string display_stack(std::stack<int> stack)
+{
     std::stringstream ss;
     ss << "{";
 
     const bool was_empty = stack.empty();
-    while (!stack.empty()) {
+    while (!stack.empty())
+    {
         ss << stack.top() << ", ";
         stack.pop();
     }
 
-    if (!was_empty) {
+    if (!was_empty)
+    {
         ss << "\b";
     }
 
     ss << "}";
 
     return ss.str();
+}
+
+void print_uint64_t(uint64_t value)
+{
+    std::stringstream ss;
+
+    for (int i = 64; i > 0; --i)
+    {
+        if (i % 4 == 0)
+        {
+            ss << " ";
+        }
+        if (i % 8 == 0)
+        {
+            ss << '\n';
+        }
+
+        if (value & (1ull << i))
+        {
+            ss << "1";
+        }
+        else
+        {
+            ss << "0";
+        }
+    }
+
+    std::cout << ss.str() << std::endl;
+}
+
+void print_uint64_t_comp(uint64_t value1, uint64_t value2, const std::string &sep)
+{
+    std::stringstream ss;
+
+    for (int r = 7; r >= 0; --r)
+    {
+        for (int c = 7; c >= 0; --c)
+        {
+            if (c % 4 == 3)
+            {
+                ss << " ";
+            }
+
+            if (value1 & (1ull << r * 8 + c))
+            {
+                ss << "1";
+            }
+            else
+            {
+                ss << "0";
+            }
+        }
+
+        ss << sep;
+
+        for (int c = 7; c >= 0; --c)
+        {
+            if (c % 4 == 3)
+            {
+                ss << " ";
+            }
+
+            if (value2 & (1ull << r * 8 + c))
+            {
+                ss << "1";
+            }
+            else
+            {
+                ss << "0";
+            }
+        }
+
+        ss << "\n";
+    }
+
+    std::cout << ss.str() << std::endl;
 }
--- tests/include/low_level_test_functions.h
+++ tests/include/low_level_test_functions.h
@@ -6,15 +6,18 @@
 #ifndef LOW_LEVEL_TEST_FUNCTIONS_H
 #define LOW_LEVEL_TEST_FUNCTIONS_H
 
-#include <vector>
-#include <stack>
+#include <cstdint>
 #include <iostream>
 #include <sstream>
+#include <stack>
 #include <string>
+#include <vector>
 
 bool stack_contains(std::stack<int> stack, int value);
-bool stack_is_equal(std::vector<int> stack1, const std::stack<int>& stack2);
+bool stack_is_equal(std::vector<int> stack1, const std::stack<int> &stack2);
 void empty_stack(std::stack<int> *stack);
 std::string display_stack(std::stack<int> stack);
+void print_uint64_t(uint64_t value);
+void print_uint64_t_comp(uint64_t value1, uint64_t value2, const std::string &sep);
 
-#endif //LOW_LEVEL_TEST_FUNCTIONS_H
+#endif // LOW_LEVEL_TEST_FUNCTIONS_H
